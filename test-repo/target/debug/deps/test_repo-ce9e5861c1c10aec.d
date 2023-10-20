4c5dfdc087068c10ad857c1055699aa188268269
Dean Longmire <107720531+DeanLongmire@users.noreply.github.com>
2023-10-20 14:46:27 -0400

    Add a rust hello world project (#1)

---
+++ test-repo/target/debug/deps/test_repo-ce9e5861c1c10aec.d
@@ -0,0 +1,5 @@
+/git/cs401/point72/test-repo/test-repo/target/debug/deps/test_repo-ce9e5861c1c10aec: src/main.rs
+
+/git/cs401/point72/test-repo/test-repo/target/debug/deps/test_repo-ce9e5861c1c10aec.d: src/main.rs
+
+src/main.rs:

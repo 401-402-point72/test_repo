ac48fdd95b6ef4937c9c7d5f7eb5e6e497202072
Dean Longmire <deanlongmire100@gmail.com>
2023-10-24 11:43:33 -0400

    Untrack target folder

--- test_repo/target/debug/deps/test_repo-779a1c352efa8694.d
+++
@@ -1,5 +0,0 @@
-/git/cs401/point72/test_repo/test_repo/target/debug/deps/test_repo-779a1c352efa8694: src/main.rs
-
-/git/cs401/point72/test_repo/test_repo/target/debug/deps/test_repo-779a1c352efa8694.d: src/main.rs
-
-src/main.rs:
--- test_repo/target/debug/deps/test_repo-d9683a9029f04623.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ryanc\OneDrive\Documents\GitHub\test_repo\test_repo\target\debug\deps\test_repo-d9683a9029f04623.rmeta: src\main.rs
-
-C:\Users\ryanc\OneDrive\Documents\GitHub\test_repo\test_repo\target\debug\deps\test_repo-d9683a9029f04623.d: src\main.rs
-
-src\main.rs:

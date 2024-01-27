34b9c97b9518906e5d5aa387e144c0671f286113
Dean Longmire <deanlongmire100@gmail.com>
2024-01-27 18:26:25 -0500

    Add retrieval of an account on a test network

--- test_repo/src/main.rs
+++ test_repo/src/main.rs
@@ -1,3 +1,32 @@
+pub mod web3 {
+    use std::env;
+    use std::str::FromStr;
+    use web3::types::{Address, H160, U256};
+
+    fn wei_to_eth(wei_val: U256) -> f64 {
+        let res = wei_val.as_u128() as f64;
+        res / 1_000_000_000_000_000_000.0
+    }
+
+    #[tokio::main]
+    pub async fn main() -> web3::Result<()> {
+        dotenv::dotenv().ok();
+
+        let websocket = web3::transports::WebSocket::new(&env::var("INFURA_SEPOLIA").unwrap()).await?;
+        let web3s = web3::Web3::new(websocket);
+
+        let mut accounts = web3s.eth().accounts().await?;
+        accounts.push(H160::from_str(&env::var("ACCOUNT_ADDRESS").unwrap()).unwrap());
+        println!("Accounts: {:?}", accounts);
+
+        for account in accounts {
+            let balance = web3s.eth().balance(account, None).await?;
+            println!("Eth balance of {:?} {}", account, wei_to_eth(balance));
+        }
+        Ok(())
+    }
+}
+
 pub mod my_module {
     pub fn greeting() -> String {
         "Hello World!".to_string()
@@ -7,6 +36,7 @@ pub mod my_module {
 fn main() {
     let message = my_module::greeting();
     println!("{}", message);
+    let _ = web3::main();
 }
 
 #[test]

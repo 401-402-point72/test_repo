f75b9366e33e9b410a9899ba13e599f71a946d40
Dean Longmire <107720531+DeanLongmire@users.noreply.github.com>
2024-01-31 13:42:12 -0500

    Improve querying (#33)
    * Cache 10 newest blocks
    
    * Fix workflow bug where run should be build
    
    * Get timestamp too cuz why not
    
    f
    
    f
    
    * refactor how we make requests
    
    f

--- test_repo/src/main.rs
+++ test_repo/src/main.rs
@@ -1,11 +1,23 @@
 pub mod web3 {
+    use chrono::{DateTime, Local, TimeZone};
+    use std::collections::HashMap;
     use std::env;
     use std::str::FromStr;
+    use std::thread;
+    use std::time::Duration;
     use web3::types::{Address, BlockId, BlockNumber, H160, U256, U64};
 
     fn wei_to_eth(wei_val: U256) -> f64 {
         let res = wei_val.as_u128() as f64;
         res / 1_000_000_000_000_000_000.0
+    }
+
+    fn convert_date(timestamp_str: &str) -> DateTime<Local> {
+        if let Ok(timestamp) = timestamp_str.parse::<i64>() {
+            return Local.timestamp(timestamp, 0);
+        } else {
+            return Local.timestamp(0, 0);
+        }
     }
 
     #[tokio::main]
@@ -28,30 +40,36 @@ pub mod web3 {
             println!("Eth balance of {:?} {}", account, wei_to_eth(balance));
         }
 
+        // Used for caching latest block number
         let mut previous_block_number: U64 = U64([u64::min_value(); 1]);
 
-        // Get the latest block
-        let latest_block = web3s
-            .eth()
-            .block(BlockId::Number(BlockNumber::Latest))
-            .await
-            .unwrap()
-            .unwrap();
+        while true {
+            // Get the latest block
+            let latest_block = web3s
+                .eth()
+                .block(BlockId::Number(BlockNumber::Latest))
+                .await
+                .unwrap()
+                .unwrap();
 
-        let blockNumber = latest_block.number.unwrap();
+            let block_number = latest_block.number.unwrap();
 
-        // Do not print block if that one was already printed
-        if blockNumber != previous_block_number {
-            println!(
-                "block number {}, number of transactions: {}, difficulty {}",
-                latest_block.number.unwrap(),
-                &latest_block.transactions.len(),
-                &latest_block.total_difficulty.unwrap()
-            );
+            // Do not print block if that one was already printed
+            if block_number > previous_block_number {
+                println!(
+                    "block number {}, number of transactions: {}, difficulty {} @ {}",
+                    latest_block.number.unwrap(),
+                    &latest_block.transactions.len(),
+                    &latest_block.total_difficulty.unwrap(),
+                    convert_date(&latest_block.timestamp.to_string())
+                );
+            }
+
+            previous_block_number = block_number;
+
+            // limits the number of requests we make
+            thread::sleep(Duration::from_secs(1));
         }
-
-        previous_block_number = blockNumber;
-
         Ok(())
     }
 }
